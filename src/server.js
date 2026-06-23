c4523feae6dd1c72f83563830afd8c01047157aa
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-06-23 19:54:22 +0530

    utils file discussion and understanding

---
+++ src/server.js
@@ -0,0 +1,21 @@
+// require("dotenv").config();
+import dotenv from "dotenv";
+import express from "express";
+import connectToDb from "./db/db.js";
+import app from "./app.js";
+
+dotenv.config({
+  path: ".env",
+});
+
+const PORT = process.env.PORT || 8000;
+
+connectToDb()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
