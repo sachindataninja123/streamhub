d550dcd89ce2f208433eb4a0330aaec7ecc0132b
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-06-26 00:17:54 +0530

    add comment, like, playlist, tweet models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const playlistSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const PlayList = mongoose.model("Playlist", playlistSchema);
+
+export default PlayList;
