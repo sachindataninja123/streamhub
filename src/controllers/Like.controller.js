24bdf0e87b7662b36e120678e4cb5fd06039cd77
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-06-27 22:49:05 +0530

    toggleTweetLike endpoint added

--- src/controllers/Like.controller.js
+++ src/controllers/Like.controller.js
@@ -5,6 +5,7 @@ import { Video } from "../models/video.model.js";
 import Like from "../models/like.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import Comment from "../models/comment.model.js";
+import Tweet from "../models/tweet.model.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
@@ -65,4 +66,38 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
     );
 });
 
-export { toggleVideoLike , toggleCommentLike };
+const toggleTweetLike = asyncHandler(async (req, res) => {
+  const tweetId = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(tweetId)) {
+    throw new ApiError(400, "TweetId is not  valid!");
+  }
+
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found!");
+  }
+
+  const likedTweet = await Like.findOne({
+    tweet: tweetId,
+    likedBy: req.user._id,
+  });
+
+  if (likedTweet) {
+    await Like.findByIdAndDelete(likedTweet._id);
+  } else {
+    await Like.create({
+      tweet: tweetId,
+      likedBy: req.user._id,
+    });
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, {}, likedTweet ? "Tweet Unliked!" : "Tweet Liked!"),
+      ""
+    );
+});
+
+export { toggleVideoLike, toggleCommentLike, toggleTweetLike };
--- src/routes/Like.routes.js
+++ src/routes/Like.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import isAuth from "../middlewares/auth.middleware.js";
 import {
   toggleCommentLike,
+  toggleTweetLike,
   toggleVideoLike,
 } from "../controllers/Like.controller.js";
 
@@ -9,5 +10,6 @@ const likeRouter = express.Router();
 
 likeRouter.post("/video/:videoId", isAuth, toggleVideoLike);
 likeRouter.post("/comment/:commentId", isAuth, toggleCommentLike);
+likeRouter.post("/tweet/:tweetId", isAuth, toggleTweetLike);
 
 export default likeRouter;
