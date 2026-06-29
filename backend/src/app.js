6dfec61ef49f457c7dbe75fd0d0e4f81314c763f
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-06-29 18:34:46 +0530

    folder setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,10 +24,6 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
 //routes
 app.use("/api/v1", healthRouter);
 app.use("/api/v1/users", userRouter);
