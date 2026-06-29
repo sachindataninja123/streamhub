2a0f9d80d0a9dfc57b245421ca1723fe9360c0dd
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-06-29 13:05:28 +0530

    tweet routes endpoint added

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,6 +1,17 @@
-import express from "express"
+import express from "express";
+import isAuth from "../middlewares/auth.middleware.js";
+import {
+  createTweet,
+  deleteTweet,
+  getUserTweets,
+  updateTweet,
+} from "../controllers/tweet.controller.js";
 
-const tweetRouter = express.Router()
+const tweetRouter = express.Router();
 
+tweetRouter.post("/create", isAuth, createTweet);
+tweetRouter.get("/user/:userId", isAuth, getUserTweets);
+tweetRouter.patch("/update/:tweetId", isAuth, updateTweet);
+tweetRouter.delete("/delete/:tweetId", isAuth, deleteTweet);
 
-export default tweetRouter
+export default tweetRouter;
