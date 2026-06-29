513669a0984d0f687a537d0735f1e0313dda47e1
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-06-29 11:21:21 +0530

    addvideosToplaylist and removeVideosfromplaylist endpoints added and testing

--- src/controllers/PlayList.controller.js
+++ src/controllers/PlayList.controller.js
@@ -1,7 +1,9 @@
+import mongoose from "mongoose";
 import PlayList from "../models/playlist.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
 
 const createPlayList = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
@@ -25,5 +27,104 @@ const createPlayList = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, playList, "PlayList created successfully!"));
 });
 
+const addVideosToPlaylist = asyncHandler(async (req, res) => {
+  const { playlistId, videoId } = req.params;
 
-export {createPlayList}
+  if (!mongoose.Types.ObjectId.isValid(playlistId)) {
+    throw new ApiError(400, "Playlist Id is invalid!");
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(videoId)) {
+    throw new ApiError(400, "video Id is invalid!");
+  }
+
+  const playList = await PlayList.findById(playlistId);
+  if (!playList) {
+    throw new ApiError(404, "playlist not found!");
+  }
+
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "video not found!");
+  }
+
+  if (playList.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "Access denied!");
+  }
+
+  if (playList.videos.includes(videoId)) {
+    throw new ApiError(400, "Video already in playlist!");
+  }
+
+  const updatedPlayList = await PlayList.findByIdAndUpdate(
+    playlistId,
+    {
+      $push: { videos: videoId },
+    },
+    {
+      new: true,
+    }
+  );
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(
+        200,
+        updatedPlayList,
+        "Video added in playlist successfully!"
+      )
+    );
+});
+
+const removeVideosFromPlaylist = asyncHandler(async (req, res) => {
+  const { playlistId, videoId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(playlistId)) {
+    throw new ApiError(400, "Playlist Id is invalid!");
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(videoId)) {
+    throw new ApiError(400, "video Id is invalid!");
+  }
+
+  const playList = await PlayList.findById(playlistId);
+  if (!playList) {
+    throw new ApiError(404, "playlist not found!");
+  }
+
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "video not found!");
+  }
+
+  if (playList.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "Access denied!");
+  }
+
+  if (!playList.videos.includes(videoId)) {
+    throw new ApiError(400, "Videos not in playlist!");
+  }
+
+  const updatedPlayList = await PlayList.findByIdAndUpdate(
+    playlistId,
+    {
+      $pull: { videos: videoId },
+    },
+    {
+      new: true,
+    }
+  );
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        updatedPlayList,
+        "Video removed from playlist successfully!"
+      )
+    );
+});
+
+export { createPlayList, addVideosToPlaylist , removeVideosFromPlaylist};
--- src/routes/playList.routes.js
+++ src/routes/playList.routes.js
@@ -1,9 +1,19 @@
 import express from "express";
 import isAuth from "../middlewares/auth.middleware.js";
-import { createPlayList } from "../controllers/PlayList.controller.js";
+import {
+  addVideosToPlaylist,
+  createPlayList,
+  removeVideosFromPlaylist,
+} from "../controllers/PlayList.controller.js";
 
 const playlistRouter = express.Router();
 
 playlistRouter.post("/create", isAuth, createPlayList);
+playlistRouter.patch("/add/:videoId/:playlistId", isAuth, addVideosToPlaylist);
+playlistRouter.patch(
+  "/remove/:videoId/:playlistId",
+  isAuth,
+  removeVideosFromPlaylist
+);
 
 export default playlistRouter;
